bb1cb658c7cbd29b2cbf3341c9d4074545fa6d0a
rpsohag <rpsohag.bd@gmail.com>
2023-06-24 20:53:40 +0600

    setup login, register, forget password page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { RouterProvider } from 'react-router-dom'
 import './App.css'
+import "./assets/css/bootstrap.min.css"
+import "./assets/css/font-awesome.min.css"
+import "./assets/css/feathericon.min.css"
+import "./assets/css/select2.min.css"
+import "./assets/css/style.css"
 import router from './router/router'
 
 function App() {
---
+++ src/pages/auth/Forgot.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import logoWhite from "./../../assets/img/logo-white.png"
+import { Link } from 'react-router-dom'
+
+const Forgot = () => {
+  return (
+    <>
+        <div class="main-wrapper login-body">
+            <div class="login-wrapper">
+            	<div class="container">
+                	<div class="loginbox">
+                    	<div class="login-left">
+							<img class="img-fluid" src={logoWhite} alt="Logo" />
+                        </div>
+                        <div class="login-right">
+							<div class="login-right-wrap">
+								<h1>Forgot Password?</h1>
+								<p class="account-subtitle">Enter your email to get a password reset link</p>
+								
+								<form action="https://dreamguys.co.in/demo/doccure/admin/login.html">
+									<div class="form-group">
+										<input class="form-control" type="text" placeholder="Email" />
+									</div>
+									<div class="form-group mb-0">
+										<button class="btn btn-primary btn-block" type="submit">Reset Password</button>
+									</div>
+								</form>
+								
+								<div class="text-center dont-have">Remember your password? <Link to="/login">Login</Link></div>
+							</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Forgot
