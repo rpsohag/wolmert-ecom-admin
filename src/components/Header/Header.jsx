525db04edbc65407a88de1cb6bbee31c102486c8
RP SOHAG <rpsohag.bd@gmail.com>
2023-07-03 21:23:27 +0600

    fix some errors

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,10 @@ import logoSmall from "./../../assets/img/logo-small.png"
 import logo from "./../../assets/img/logo.png"
 import avatar1 from "./../../assets/img/profiles/avatar-01.jpg"
 import doc from "./../../assets/img/doctors/doctor-thumb-01.jpg"
-import useDropdownPopupControl from '../../hooks/useDropdownPopupControl'
+
 
 
 const Header = () => {
-  const {isOpen, toggleMenu, dropDownRef} = useDropdownPopupControl()
-  const {isOpen : isNotification, toggleMenu : notificationMenu, dropDownRef : notificationRef} = useDropdownPopupControl()
   return (
     <>
      <div className="header">
@@ -23,14 +21,14 @@ const Header = () => {
 </a>
     </div>
 
-<a href="javascript:void(0);" id="toggle_btn">
+<a href="" id="toggle_btn">
 <i className="fe fe-text-align-left"></i>
 </a>
 
 <div className="top-nav-search">
 <form>
 <input type="text" className="form-control" placeholder="Search here" />
-<button className="btn" type="submit"><i className="fa fa-search"></i></button>
+<button className="btn" type="submit"><i className="fe fe-search"></i></button>
 </form>
 </div>
 
@@ -42,97 +40,94 @@ const Header = () => {
 
 
 <ul className="nav user-menu">
-
-<li className="nav-item dropdown noti-dropdown" ref={notificationRef}>
-<a href="#" className="dropdown-toggle nav-link" data-toggle="dropdown" onClick={notificationMenu}>
-  <i className="fe fe-bell"></i> <span className="badge badge-pill">3</span>
-</a>
-{ isNotification && 
-<div className="dropdown-menu notifications d-block" style={{ transform : "translate(-298px, 0px)"}}>
-  <div className="topnav-dropdown-header">
-    <span className="notification-title">Notifications</span>
-    <a href="javascript:void(0)" className="clear-noti"> Clear All </a>
+<li className="nav-item dropdown noti-dropdown">
+  <a href="#" className="dropdown-toggle nav-link" data-toggle="dropdown">
+    <i className="fe fe-bell"></i> <span className="badge badge-pill">3</span>
+  </a>
+  <div className="dropdown-menu notifications">
+    <div className="topnav-dropdown-header">
+      <span className="notification-title">Notifications</span>
+      <a href="" className="clear-noti"> Clear All </a>
+    </div>
+    <div className="noti-content">
+      <ul className="notification-list">
+        <li className="notification-message">
+          <a href="#">
+            <div className="media">
+              <span className="avatar avatar-sm">
+                <img className="avatar-img rounded-circle" alt="User Image" src={doc} />
+              </span>
+              <div className="media-body">
+                <p className="noti-details"><span className="noti-title">Dr. Ruby Perrin</span> Schedule <span className="noti-title">her appointment</span></p>
+                <p className="noti-time"><span className="notification-time">4 mins ago</span></p>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li className="notification-message">
+          <a href="#">
+            <div className="media">
+              <span className="avatar avatar-sm">
+                <img className="avatar-img rounded-circle" alt="User Image" src={doc} />
+              </span>
+              <div className="media-body">
+                <p className="noti-details"><span className="noti-title">Charlene Reed</span> has booked her appointment to <span className="noti-title">Dr. Ruby Perrin</span></p>
+                <p className="noti-time"><span className="notification-time">6 mins ago</span></p>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li className="notification-message">
+          <a href="#">
+            <div className="media">
+              <span className="avatar avatar-sm">
+                <img className="avatar-img rounded-circle" alt="User Image" src={doc} />
+              </span>
+              <div className="media-body">
+              <p className="noti-details"><span className="noti-title">Travis Trimble</span> sent a amount of $210 for his <span className="noti-title">appointment</span></p>
+              <p className="noti-time"><span className="notification-time">8 mins ago</span></p>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li className="notification-message">
+          <a href="#">
+            <div className="media">
+              <span className="avatar avatar-sm">
+                <img className="avatar-img rounded-circle" alt="User Image" src={doc} />
+              </span>
+              <div className="media-body">
+                <p className="noti-details"><span className="noti-title">Carl Kelly</span> send a message <span className="noti-title"> to his doctor</span></p>
+                <p className="noti-time"><span className="notification-time">12 mins ago</span></p>
+              </div>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div className="topnav-dropdown-footer">
+      <a href="#">View all Notifications</a>
+    </div>
   </div>
-  <div className="noti-content">
-    <ul className="notification-list">
-      <li className="notification-message">
-        <a href="#">
-          <div className="media">
-            <span className="avatar avatar-sm">
-              <img className="avatar-img rounded-circle" alt="User Image" src={doc} />
-            </span>
-            <div className="media-body">
-              <p className="noti-details"><span className="noti-title">Dr. Ruby Perrin</span> Schedule <span className="noti-title">her appointment</span></p>
-              <p className="noti-time"><span className="notification-time">4 mins ago</span></p>
-            </div>
-          </div>
-        </a>
-      </li>
-      <li className="notification-message">
-        <a href="#">
-          <div className="media">
-            <span className="avatar avatar-sm">
-              <img className="avatar-img rounded-circle" alt="User Image" src={doc} />
-            </span>
-            <div className="media-body">
-              <p className="noti-details"><span className="noti-title">Charlene Reed</span> has booked her appointment to <span className="noti-title">Dr. Ruby Perrin</span></p>
-              <p className="noti-time"><span className="notification-time">6 mins ago</span></p>
-            </div>
-          </div>
-        </a>
-      </li>
-      <li className="notification-message">
-        <a href="#">
-          <div className="media">
-            <span className="avatar avatar-sm">
-              <img className="avatar-img rounded-circle" alt="User Image" src={doc} />
-            </span>
-            <div className="media-body">
-            <p className="noti-details"><span className="noti-title">Travis Trimble</span> sent a amount of $210 for his <span className="noti-title">appointment</span></p>
-            <p className="noti-time"><span className="notification-time">8 mins ago</span></p>
-            </div>
-          </div>
-        </a>
-      </li>
-      <li className="notification-message">
-        <a href="#">
-          <div className="media">
-            <span className="avatar avatar-sm">
-              <img className="avatar-img rounded-circle" alt="User Image" src={doc} />
-            </span>
-            <div className="media-body">
-              <p className="noti-details"><span className="noti-title">Carl Kelly</span> send a message <span className="noti-title"> to his doctor</span></p>
-              <p className="noti-time"><span className="notification-time">12 mins ago</span></p>
-            </div>
-          </div>
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div className="topnav-dropdown-footer">
-    <a href="#">View all Notifications</a>
-  </div>
-</div>
-}
 </li>
-<li className="nav-item dropdown has-arrow" ref={dropDownRef}>
-<a href="#" className="dropdown-toggle nav-link" data-toggle="dropdown" onClick={toggleMenu}>
-  <span className="user-img"><img className="rounded-circle" src={avatar1} width="31" alt="Ryan Taylor" /></span>
-</a>
-{ isOpen && <div className="dropdown-menu d-block" style={{ transform : "translate(-139px, 0px)"}}>
-  <div className="user-header">
-    <div className="avatar avatar-sm">
-      <img src={avatar1} alt="User Image" className="avatar-img rounded-circle" />
+<li className="nav-item dropdown has-arrow">
+  <a href="#" className="dropdown-toggle nav-link" data-toggle="dropdown">
+    <span className="user-img"><img className="rounded-circle" src={avatar1} width="31" alt="Ryan Taylor" /></span>
+  </a>
+  <div className="dropdown-menu">
+    <div className="user-header">
+      <div className="avatar avatar-sm">
+        <img src={avatar1} alt="User Image" className="avatar-img rounded-circle" />
+      </div>
+      <div className="user-text">
+        <h6>Ryan Taylor</h6>
+        <p className="text-muted mb-0">Administrator</p>
+      </div>
     </div>
-    <div className="user-text">
-      <h6>Ryan Taylor</h6>
-      <p className="text-muted mb-0">Administrator</p>
-    </div>
+    <a className="dropdown-item" href="profile.html">My Profile</a>
+    <a className="dropdown-item" href="settings.html">Settings</a>
+    <a className="dropdown-item" href="login.html">Logout</a>
   </div>
-  <a className="dropdown-item" href="profile.html">My Profile</a>
-  <a className="dropdown-item" href="settings.html">Settings</a>
-  <a className="dropdown-item" href="login.html">Logout</a>
-</div>}
 </li>
 </ul>
 </div>
