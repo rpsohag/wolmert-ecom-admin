a582911bf33d706ec6e6f2711900463cf61cc4da
RP SOHAG <rpsohag.bd@gmail.com>
2023-07-15 21:16:31 +0600

    getting input data from login register page

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,49 +1,82 @@
-import React from 'react'
-import logoWhite from './../../assets/img/logo-white.png'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import logoWhite from "./../../assets/img/logo-white.png";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+  });
+  const handleInputChange = (e) => {
+    setInput((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
   return (
     <>
-        <div className="main-wrapper login-body">
-            <div className="login-wrapper">
-            	<div className="container">
-                	<div className="loginbox">
-                    	<div className="login-left">
-							<img className="img-fluid" src={logoWhite} alt="Logo" />
-                        </div>
-                        <div className="login-right">
-							<div className="login-right-wrap">
-								<h1>Login</h1>
-								<p className="account-subtitle">Access to our dashboard</p>
-								
-								<form action="https://dreamguys.co.in/demo/doccure/admin/index.html">
-									<div className="form-group">
-										<input className="form-control" type="text" placeholder="Email" />
-									</div>
-									<div className="form-group">
-										<input className="form-control" type="text" placeholder="Password" />
-									</div>
-									<div className="form-group">
-										<button className="btn btn-primary btn-block" type="submit">Login</button>
-									</div>
-								</form>
-								
-								<div className="text-center forgotpass"><Link to="/forgot">Forgot Password?</Link></div>
-								<div className="login-or">
-									<span className="or-line"></span>
-									<span className="span-or">or</span>
-								</div>
-								  
-								<div className="text-center dont-have">Don’t have an account? <Link to="/register">Register</Link></div>
-							</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </>
-  )
-}
+      <div className="main-wrapper login-body">
+        <div className="login-wrapper">
+          <div className="container">
+            <div className="loginbox">
+              <div className="login-left">
+                <img className="img-fluid" src={logoWhite} alt="Logo" />
+              </div>
+              <div className="login-right">
+                <div className="login-right-wrap">
+                  <h1>Login</h1>
+                  <p className="account-subtitle">Access to our dashboard</p>
 
-export default Login
+                  <form>
+                    <div className="form-group">
+                      <input
+                        className="form-control"
+                        type="text"
+                        placeholder="Email"
+                        name="email"
+                        value={input.email}
+                        onChange={handleInputChange}
+                      />
+                    </div>
+                    <div className="form-group">
+                      <input
+                        className="form-control"
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={input.password}
+                        onChange={handleInputChange}
+                      />
+                    </div>
+                    <div className="form-group">
+                      <button
+                        className="btn btn-primary btn-block"
+                        type="submit"
+                      >
+                        Login
+                      </button>
+                    </div>
+                  </form>
+
+                  <div className="text-center forgotpass">
+                    <Link to="/forgot">Forgot Password?</Link>
+                  </div>
+                  <div className="login-or">
+                    <span className="or-line"></span>
+                    <span className="span-or">or</span>
+                  </div>
+
+                  <div className="text-center dont-have">
+                    Don’t have an account? <Link to="/register">Register</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
